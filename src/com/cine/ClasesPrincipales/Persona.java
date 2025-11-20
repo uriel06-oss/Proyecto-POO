d2e1a88d903f0df341ab4dfd0c07cd4a222a3b4d
Tiago3600 <tiagobarrp@gmail.com>
2025-11-20 11:23:09 -0600

    Add edad field and its getters/setters to Persona

--- src/com/cine/ClasesPrincipales/Persona.java
+++ src/com/cine/ClasesPrincipales/Persona.java
@@ -9,15 +9,15 @@ public abstract class Persona {
     private String contraseña;
     private String correo;
     private String celular;
+    private String edad;
 
     public Persona() {
-
         this.nombre = "Si ves esto es pq algo hicimos mal:c";
-
     }
 
-    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno, String nickname, String contraseña,
-            String correo, String celular) {
+    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno,
+                   String nickname, String contraseña, String correo,
+                   String celular, String edad) {
 
         this.nombre = nombre;
         this.apellidoPaterno = apellidoPaterno;
@@ -26,7 +26,70 @@ public abstract class Persona {
         this.contraseña = contraseña;
         this.correo = correo;
         this.celular = celular;
-
+        this.edad = edad;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
 }
